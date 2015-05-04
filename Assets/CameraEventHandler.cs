ee37e7f68929e27ef04b997b141e1818ca39a1e8
travisjlewis <travis.lewis@students.olin.edu>
2015-05-04 03:45:00 -0400

    smoothing shake

--- Assets/CameraEventHandler.cs
+++ Assets/CameraEventHandler.cs
@@ -21,8 +21,6 @@ public class CameraEventHandler : MonoBehaviour {
 			Destroy (gameObject);
 		} else {
 			G = this;
-			// TODO: think about lifecycle
-			//DontDestroyOnLoad (gameObject);
 		}
 	}
 	
@@ -35,6 +33,8 @@ public class CameraEventHandler : MonoBehaviour {
 	public GameObject cameraTarget;
 	public GameObject blindPanel;
 	public GameObject redFlashPanel;
+
+	public float fadeInTime = 0.5f;
 	#endregion
 
 	#region Cache Variables
@@ -64,17 +64,18 @@ public class CameraEventHandler : MonoBehaviour {
 
 		float shakeCounter = 0.0f;
 		float shakeAngle = 0.0f;
+		float currentAngle = 0.0f;
 		Quaternion initialRotation = myCameraTransform.rotation;
 
 		while(counter > 0.0f)
 		{
-			if(shakeCounter > 0.06f)
+			//TODO: How often we shake should be related to intensity.
+			if(shakeCounter > 0.02f)
 			{
 				shakeCounter = 0.0f;
 				shakeAngle = (shakeAngle + Mathf.PI * 0.7f) % (Mathf.PI * 2f);
-				//TODO: Lerp between these values, instead of instantly jerking to that angle.
-				myCameraTransform.rotation *= Quaternion.Euler(Mathf.Cos(shakeAngle) * intensity, Mathf.Sin(shakeAngle) * intensity, 0.0f);
 			}
+			myCameraTransform.rotation *= Quaternion.Euler(Mathf.Cos(shakeAngle) * intensity, Mathf.Sin(shakeAngle) * intensity, 0.0f);
 			shakeCounter += Time.deltaTime;
 			counter -= Time.deltaTime;
 			yield return null;
@@ -91,8 +92,6 @@ public class CameraEventHandler : MonoBehaviour {
 		float counter = time;
 		
 		float fadeCounter = 0f;
-		//TODO: Move this to a public variable
-		float fadeTime = 0.5f;
 
 		Image flashImage = redFlashPanel.GetComponent<Image> ();
 
@@ -101,9 +100,9 @@ public class CameraEventHandler : MonoBehaviour {
 		float pingPongOffset = intensity - pingPongRange;
 
 		//Lerp it in
-		while(fadeCounter < fadeTime)
+		while(fadeCounter < fadeInTime)
 		{
-			flashImage.color = new Color(1, 1, 1, Mathf.Lerp(0.0f, intensity, (fadeCounter / fadeTime)));
+			flashImage.color = new Color(1, 1, 1, Mathf.Lerp(0.0f, intensity, (fadeCounter / fadeInTime)));
 			fadeCounter += Time.deltaTime;
 			yield return null;
 		}
@@ -122,9 +121,9 @@ public class CameraEventHandler : MonoBehaviour {
 			}
 			
 			//And lerp it back.
-			while(fadeCounter < fadeTime)
+			while(fadeCounter < fadeInTime)
 			{
-				flashImage.color = new Color(1, 1, 1, Mathf.Lerp(intensity, 0.0f, (fadeCounter / fadeTime)));
+				flashImage.color = new Color(1, 1, 1, Mathf.Lerp(intensity, 0.0f, (fadeCounter / fadeInTime)));
 				fadeCounter += Time.deltaTime;
 				yield return null;
 			}
@@ -139,8 +138,6 @@ public class CameraEventHandler : MonoBehaviour {
 		float counter = time;
 
 		float fadeCounter = 0f;
-		//TODO: Move this to a public variable
-		float fadeTime = 0.5f;
 
 		//Image.CrossFadeAlpha is so hilariously broken. Fading to a higher alpha just plain DOESN'T work. So I have to lerp it myself.
 		//Also, set the color to full black in code, so that it doesn't completely block the view in the editor
@@ -148,9 +145,9 @@ public class CameraEventHandler : MonoBehaviour {
 		CanvasRenderer blindImageRenderer = blindImage.canvasRenderer;
 
 		//Lerp it in
-		while(fadeCounter < fadeTime)
+		while(fadeCounter < fadeInTime)
 		{
-			blindImage.color = new Color(0, 0, 0, Mathf.Lerp(0.0f, intensity, (fadeCounter / fadeTime)));
+			blindImage.color = new Color(0, 0, 0, Mathf.Lerp(0.0f, intensity, (fadeCounter / fadeInTime)));
 			fadeCounter += Time.deltaTime;
 			yield return null;
 		}
@@ -165,9 +162,9 @@ public class CameraEventHandler : MonoBehaviour {
 		}
 
 		//And lerp it back.
-		while(fadeCounter < fadeTime)
+		while(fadeCounter < fadeInTime)
 		{
-			blindImage.color = new Color(0, 0, 0, Mathf.Lerp(intensity	, 0.0f, (fadeCounter / fadeTime)));
+			blindImage.color = new Color(0, 0, 0, Mathf.Lerp(intensity	, 0.0f, (fadeCounter / fadeInTime)));
 			fadeCounter += Time.deltaTime;
 			yield return null;
 		}
--- Assets/TestShake.cs
+++ Assets/TestShake.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class TestShake : MonoBehaviour {
 
-	bool fired = false;
+	float timer = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,10 +12,11 @@ public class TestShake : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(!fired)
-		{
-			CameraEventHandler.G.FireEvent(CameraEvent.Shake, 0.5f);
-			fired = true;
+		if (timer < 0.0f) {
+			CameraEventHandler.G.FireEvent (CameraEvent.Shake, 0.1f);
+			timer = 1.0f;
 		}
+
+		timer -= Time.deltaTime;
 	}
 }
